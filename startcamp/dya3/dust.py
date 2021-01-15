94f34da97e6d97210caa660ffaf35fd3abe3611b
test gyun <indd888@gmail.com>
2021-01-15 16:34:33 +0900

    텔레그램 메시지 전송 코드 추가

--- startcamp/dya3/dust.py
+++ startcamp/dya3/dust.py
@@ -12,13 +12,22 @@ import requests
 
 key = 'SX4%2BNhnh5roVUuhTysQjXGOOW27422y%2BQ5Ug26waZlGAfilWbnrOo5KnZZLk4KofiqR%2FKUGv%2BiaNG5g2XIviLQ%3D%3D'
 
-url = f'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey={key}&sidoName=전남&returnType=json'
+url = f'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey={key}&sidoName=광주&returnType=json'
 
 data1 = requests.get(url).json()
 
-name = data1['response']['body']['items'][26]['stationName']
-value = data1['response']['body']['items'][26]['pm10Value']
-sidoName = data1['response']['body']['items'][26]['sidoName']
-print(f'{sidoName} {name}의 미세먼지 농도는 {value}입니다.')
+name = data1['response']['body']['items'][1]['stationName']
+value = data1['response']['body']['items'][1]['pm10Value']
+sidoName = data1['response']['body']['items'][1]['sidoName']
+text = f'{sidoName} {name}의 미세먼지 농도는 {value}입니다.'
 
+
+#5. 자신에게 텔레그램 메시지 전송 (sendMessage)
+
+token = ''
+chat_id = ''
+
+telegram_url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={text}'
+
+requests.get(telegram_url)
 
